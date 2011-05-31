864bd6f2d06acebd829bedf4aa4d22fe8e106959
Nando Vieira <fnando.vieira@gmail.com>
2011-05-31 16:51:13 -0300

    Dropped Jeweler; using Bundler instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,16 @@
-require "rspec"
+require "bundler"
+Bundler.setup
+Bundler.require(:default, :development)
+
 require "storage"
-require "fileutils"
 require "pathname"
 
 TMP = Pathname.new(File.expand_path(File.dirname(__FILE__) + "/tmp"))
 RESOURCES = Pathname.new(File.expand_path(File.dirname(__FILE__) + "/resources"))
 
 RSpec.configure do |config|
-  config.formatter = "documentation"
-  config.color_enabled = true
-
-  config.before :each do
+  config.around :each do
     FileUtils.rm_rf(TMP) rescue nil
     FileUtils.mkdir_p(TMP) rescue nil
-  end
-
-  config.after :each do
-    FileUtils.rm_rf(TMP) rescue nil
   end
 end

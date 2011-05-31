79c2b2603f1709061aad416136e5d184777f3eac
Nando Vieira <fnando.vieira@gmail.com>
2011-05-31 16:54:09 -0300

    Proper RSpec usage.

--- spec/storage/strategies/file_system_spec.rb
+++ spec/storage/strategies/file_system_spec.rb
@@ -4,56 +4,56 @@ describe Storage::Strategies::FileSystem do
   before do
     @source = RESOURCES.join("file.txt")
     @destiny = TMP.join("lorem.txt")
-    
-    Storage.setup do |c| 
+
+    Storage.setup do |c|
       c.strategy = :file
       c.path = TMP
     end
   end
-  
+
   it "should save a file using file handler" do
     handler = File.open(@source)
     Storage.store(handler, :name => "lorem.txt")
-    
+
     File.should be_file(@destiny)
     File.read(@destiny).should == File.read(@source)
   end
-  
+
   it "should save a file using a path" do
     Storage.store(@source, :name => "lorem.txt")
-    
+
     File.should be_file(@destiny)
     File.read(@destiny).should == File.read(@source)
   end
-  
+
   it "should remove an existing file" do
     Storage.store(@source, :name => "lorem.txt")
     Storage.remove("lorem.txt").should be_true
     File.should_not be_file(@destiny)
   end
-  
+
   it "should raise when trying to removing an unexesting file" do
-    lambda {
+    expect {
       Storage.remove("invalid")
-    }.should raise_error(Storage::MissingFileError)
+    }.to raise_error(Storage::MissingFileError)
   end
-  
+
   it "should retrieve an existing file" do
     Storage.store(@source, :name => "lorem.txt")
     Storage.get("lorem.txt").should == File.expand_path(TMP.join("lorem.txt"))
   end
-  
+
   it "should raise when trying to retrieve an unexesting file" do
-    lambda {
+    expect {
       Storage.get("invalid")
-    }.should raise_error(Storage::MissingFileError)
+    }.to raise_error(Storage::MissingFileError)
   end
-  
+
   it "should raise when saving a file that already exists" do
     Storage.store(@source, :name => "lorem.txt")
-    
-    lambda {
+
+    expect {
       Storage.store(@source, :name => "lorem.txt")
-    }.should raise_error(Storage::FileAlreadyExistsError)
+    }.to raise_error(Storage::FileAlreadyExistsError)
   end
 end
--- spec/storage/strategies/s3_spec.rb
+++ spec/storage/strategies/s3_spec.rb
@@ -5,88 +5,88 @@ describe Storage::Strategies::S3 do
     @source = RESOURCES.join("file.txt")
     @destiny = TMP.join("lorem.txt")
     @bucket = mock("bucket", :name => "files")
-    
-    AWS::S3::Base.stub!(:establish_connection!)
-    AWS::S3::Bucket.stub!(:find).and_return(@bucket)
-    AWS::S3::S3Object.stub!(:store)
-    
-    Storage.setup do |c| 
+
+    AWS::S3::Base.stub :establish_connection!
+    AWS::S3::Bucket.stub :find => @bucket
+    AWS::S3::S3Object.stub :store
+
+    Storage.setup do |c|
       c.strategy = :s3
       c.access_key = "abc"
       c.secret_key = "123"
     end
   end
-  
+
   it "should establish connection" do
     options = {:access_key_id => "abc", :secret_access_key => "123"}
     AWS::S3::Base.should_receive(:establish_connection!).with(options)
-    
+
     Storage::Strategies::S3.prepare!
   end
-  
+
   it "should not reconnect when a connection is already established" do
     AWS::S3::Base.should_receive(:connected?).and_return(true)
     AWS::S3::Base.should_not_receive(:establish_connection!)
-    
+
     Storage::Strategies::S3.prepare!
   end
-  
+
   it "should save a file using file handler" do
-    handler = File.open(@source)    
+    handler = File.open(@source)
     AWS::S3::S3Object.should_receive(:store).with("lorem.txt", handler, "files", :access => :public_read)
     Storage.store(handler, :name => "lorem.txt", :bucket => "files")
   end
-  
+
   it "should save a file using a path" do
     AWS::S3::S3Object.should_receive(:store).with("lorem.txt", kind_of(File), "files", :access => :public_read)
     Storage.store(@source, :name => "lorem.txt", :bucket => "files")
   end
-  
+
   it "should remove an existing file" do
     object = mock("object")
     object.should_receive(:delete).and_return(true)
     Storage::Strategies::S3.should_receive(:find_object).with("lorem.txt", :bucket => "files").and_return(object)
-    
+
     Storage.remove("lorem.txt", :bucket => "files").should be_true
   end
-  
+
   it "should raise when trying to removing an unexesting file" do
     pending "spec is failing but real code works"
-    
+
     Storage::Strategies::S3.should_receive(:find_object).and_raise(AWS::S3::NoSuchKey)
-    
-    lambda {
+
+    expect {
       Storage.remove("lorem.txt", :bucket => "files")
-    }.should raise_error(Storage::MissingFileError)
+    }.to raise_error(Storage::MissingFileError)
   end
-  
+
   it "should retrieve an existing file" do
     object = mock("object")
-    
+
     AWS::S3::S3Object.should_receive(:find).with("lorem.txt", "files").and_return(object)
     AWS::S3::S3Object.should_receive(:url_for).with("lorem.txt", "files", :authenticated => false)
-    
+
     Storage.get("lorem.txt", :bucket => "files")
   end
-  
+
   it "should raise when trying to retrieve an unexesting file" do
     pending "spec is failing but real code works"
-    
+
     AWS::S3::S3Object.should_receive(:find).with("lorem.txt", "files").and_raise(AWS::S3::NoSuchKey)
     AWS::S3::S3Object.should_not_receive(:url_for)
-    
-    lambda {
+
+    expect {
       Storage.get("lorem.txt", :bucket => "files")
-    }.should raise_error(Storage::MissingFileError)
+    }.to raise_error(Storage::MissingFileError)
   end
-  
+
   it "should raise when saving a file that already exists" do
     object = mock("object")
     options = {:name => "lorem.txt", :bucket => "files"}
     Storage::Strategies::S3.should_receive(:find_object).with(@source, options).and_return(object)
-    
-    lambda {
+
+    expect {
       Storage.store(@source, :name => "lorem.txt", :bucket => "files")
-    }.should raise_error(Storage::FileAlreadyExistsError)
+    }.to raise_error(Storage::FileAlreadyExistsError)
   end
 end

571920c67908b7e1a2069487691f165369bacfe5
Nando Vieira <fnando.vieira@gmail.com>
2016-03-03 08:37:00 -0300

    Fix inverted permission issue.

--- lib/storage/strategies/s3.rb
+++ lib/storage/strategies/s3.rb
@@ -6,12 +6,12 @@ module Storage
       MissingBucket = Class.new(StandardError)
 
       def connection
-        @connection ||= Fog::Storage.new({
-          :provider                 => 'AWS',
-          :aws_access_key_id        => Storage::Config.access_key,
-          :aws_secret_access_key    => Storage::Config.secret_key,
-          :region                   => Storage::Config.region
-        })
+        @connection ||= Fog::Storage.new(
+          provider: "AWS",
+          aws_access_key_id: Storage::Config.access_key,
+          aws_secret_access_key: Storage::Config.secret_key,
+          region: Storage::Config.region
+        )
       end
 
       def prepare!
@@ -33,7 +33,7 @@ module Storage
 
       def store(file, options = {})
         object = find_object(file, options) rescue nil
-        raise FileAlreadyExistsError if object
+        fail FileAlreadyExistsError if object
 
         bucket = find_bucket_or_create(options.fetch(:bucket))
         file = File.open(file, "rb") unless file.respond_to?(:read) && !file.kind_of?(Pathname)
@@ -51,28 +51,28 @@ module Storage
       end
 
       def find_bucket!(name)
-        find_bucket(name) || raise(MissingBucket)
+        find_bucket(name) || fail(MissingBucket)
       end
 
       def create_bucket(name)
         connection.directories.create(
-          :key    => name,
-          :public => false
+          key: name,
+          public: false
         )
       end
 
       def create_object(bucket, file, options)
-        bucket.files.create({
-          :key => options.fetch(:name),
-          :body => file,
-          :public => (options[:public] || options[:access] != :public_read)
-        })
+        bucket.files.create(
+          key: options.fetch(:name),
+          body: file,
+          public: (options[:public] || options[:access] == :public_read)
+        )
       end
 
       def find_object(file, options = {})
         path = options.fetch(:name, file)
         bucket = find_bucket!(options.fetch(:bucket))
-        bucket.files.get(path) || raise(MissingFileError)
+        bucket.files.get(path) || fail(MissingFileError)
       rescue MissingBucket
         raise MissingFileError
       end
--- spec/storage/strategies/s3_spec.rb
+++ spec/storage/strategies/s3_spec.rb
@@ -22,14 +22,14 @@ describe Storage::Strategies::S3 do
   end
 
   context "general" do
-    before do
-      @adapter = Storage::Strategies::S3
-      @source = RESOURCES.join("file.txt")
-      @destiny = TMP.join("lorem.txt")
-      @connection = double("connection")
-      @bucket = double("bucket")
+    let(:adapter) { Storage::Strategies::S3 }
+    let(:source) { RESOURCES.join("file.txt") }
+    let(:destiny) { TMP.join("lorem.txt") }
+    let(:connection) { double("connection") }
+    let(:bucket) { double("bucket") }
 
-      allow(@adapter).to receive(:connection).and_return(@connection)
+    before do
+      allow(adapter).to receive(:connection).and_return(connection)
 
       Storage.setup do |c|
         c.strategy = :s3
@@ -40,95 +40,141 @@ describe Storage::Strategies::S3 do
     end
 
     it "should save a file using file handler" do
-      handler = File.open(@source)
+      handler = File.open(source)
 
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@adapter).to receive_message_chain('find_object').and_return(nil)
-      expect(@bucket).to receive_message_chain('files.create').with(:key => 'lorem.txt', :body => handler, :public => true)
+      setup_expectations
 
-      Storage.store(handler, :name => "lorem.txt", :bucket => "files")
+      Storage.store(handler, name: "lorem.txt", bucket: "files")
     end
 
     it "should save a file using a path" do
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@adapter).to receive_message_chain('find_object').and_return(nil)
-      expect(@bucket).to receive_message_chain('files.create').with(:key => 'lorem.txt', :body => kind_of(File), :public => true)
+      setup_expectations
 
-      Storage.store(@source, :name => "lorem.txt", :bucket => "files")
+      Storage.store(source, name: "lorem.txt", bucket: "files")
     end
 
     it "should remove an existing file" do
       object = double("object")
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(object)
+
+      setup_bucket(bucket)
+      setup_object(object)
       expect(object).to receive(:destroy).and_return(true)
 
-      expect(Storage.remove("lorem.txt", :bucket => "files")).to be_truthy
+      expect(Storage.remove("lorem.txt", bucket: "files")).to be_truthy
     end
 
-    it "should raise when trying to removing an unexesting file" do
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(nil)
+    it "should raise when trying to removing an nonexisting file" do
+      setup_bucket(bucket)
+      setup_get(nil)
 
       expect {
-        Storage.remove("lorem.txt", :bucket => "files")
+        Storage.remove("lorem.txt", bucket: "files")
       }.to raise_error(Storage::MissingFileError)
     end
 
     it "should retrieve an existing file (public url)" do
-      object = double("object", public_url: 'PUBLIC_URL')
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(object)
+      object = double("object", public_url: "PUBLIC_URL")
 
-      expect(Storage.get("lorem.txt", :bucket => "files")).to eq('PUBLIC_URL')
+      setup_bucket(bucket)
+      setup_object(object)
+
+      expect(Storage.get("lorem.txt", bucket: "files")).to eq("PUBLIC_URL")
     end
 
     it "should retrieve an existing file (private url)" do
       object = double("object", public_url: nil)
 
-      expect(object).to receive_message_chain('url').with(Time.now.to_i + 3600).and_return('PRIVATE_URL')
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(object)
+      expect(object).to receive_message_chain("url").with(Time.now.to_i + 3600).and_return("PRIVATE_URL")
+      setup_bucket(bucket)
+      setup_object(object)
 
-      expect(Storage.get("lorem.txt", :bucket => "files")).to eq('PRIVATE_URL')
+      expect(Storage.get("lorem.txt", :bucket => "files")).to eq("PRIVATE_URL")
     end
 
     it "should raise when trying to retrieve an missing file" do
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(nil)
+      setup_bucket(bucket)
+      setup_get(nil)
 
       expect {
-        Storage.get("lorem.txt", :bucket => "files")
+        Storage.get("lorem.txt", bucket: "files")
       }.to raise_error(Storage::MissingFileError)
     end
 
     it "should raise when trying to retrieve an missing bucket" do
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(nil)
+      setup_bucket(nil)
 
       expect {
-        Storage.get("lorem.txt", :bucket => "files")
+        Storage.get("lorem.txt", bucket: "files")
       }.to raise_error(Storage::MissingFileError)
     end
 
     it "should create a bucket and trying to store a file on a missing bucket" do
-      expect(@adapter).to receive_message_chain('find_object').and_return(nil)
-      allow(@connection).to receive_message_chain('directories.get').and_return(nil)
-      expect(@connection).to receive_message_chain('directories.create').with(key: 'files', public: false).and_return(@bucket)
+      setup_object
+      setup_bucket(nil)
 
-      @bucket.as_null_object
+      expect(connection).to receive_message_chain("directories.create").with(key: "files", public: false).and_return(bucket)
 
-      Storage.store(@source, :name => 'lorem.txt', :bucket => "files")
+      bucket.as_null_object
+
+      Storage.store(source, name: "lorem.txt", bucket: "files")
     end
 
     it "should raise when saving a file that already exists" do
       object = double("object")
 
-      expect(@connection).to receive_message_chain('directories.get').with('files').and_return(@bucket)
-      expect(@bucket).to receive_message_chain('files.get').with('lorem.txt').and_return(object)
+      setup_bucket(bucket)
+      setup_get(object)
 
       expect {
-        Storage.store(@source, :name => 'lorem.txt', :bucket => 'files')
+        Storage.store(source, name: "lorem.txt", bucket: "files")
       }.to raise_error(Storage::FileAlreadyExistsError)
+    end
+
+    it "should set file permission to public" do
+      setup_expectations public_access: true
+      Storage.store(source, name: "lorem.txt", bucket: "files", public: true)
+    end
+
+    it "should set file permission to private (default)" do
+      setup_expectations public_access: false
+      Storage.store(source, name: "lorem.txt", bucket: "files")
+    end
+
+    it "should set file permission to private" do
+      setup_expectations public_access: false
+      Storage.store(source, name: "lorem.txt", bucket: "files", public: false)
+    end
+
+    it "should set file permission to private (access option)" do
+      setup_expectations public_access: false
+      Storage.store(source, name: "lorem.txt", bucket: "files", access: :private)
+    end
+
+    it "should set file permission to public (access option)" do
+      setup_expectations public_access: true
+      Storage.store(source, name: "lorem.txt", bucket: "files", access: :public_read)
+    end
+
+    def setup_bucket(bucket)
+      allow(connection).to receive_message_chain("directories.get").with("files").and_return(bucket)
+    end
+
+    def setup_object(object = nil)
+      expect(adapter).to receive_message_chain("find_object").and_return(object)
+    end
+
+    def setup_create(public_access)
+      expect(bucket).to receive_message_chain("files.create").with(key: "lorem.txt", body: kind_of(File), public: public_access)
+    end
+
+    def setup_get(object)
+      expect(bucket).to receive_message_chain("files.get").with("lorem.txt").and_return(object)
+    end
+
+    def setup_expectations(the_bucket: bucket, object: nil, public_access: false)
+      setup_bucket(the_bucket)
+      setup_object(object)
+      setup_create(public_access)
     end
   end
 end
